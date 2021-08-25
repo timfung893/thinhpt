732ce0e62ef9b90f9cebdeeb7a1d77459d4318d9
timfung <thinhpt.sp@gmail.com>
2021-08-25 16:09:38 +0700

    update

---
+++ js/play.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    console.log('ok');
+    const menuToggle = document.querySelector('.toggle');
+    const menu = document.querySelector('.menu');
+
+    menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active');
+        menu.classList.toggle('active');
+    });
+});
