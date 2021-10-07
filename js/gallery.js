95193e5c4c1cfcbd53c252ce8dfc8e414eeb163c
timfung <thinhpt.sp@gmail.com>
2021-10-07 22:17:07 +0700

    update-lightbox

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,7 @@
 $("document").ready(() => {
   console.log("ok");
 
-  // NAV
+  // Nav
 
   const Menu = document.querySelector(".nav-bar");
   const MenuOpen = document.querySelector(".nav-icon");
@@ -13,6 +13,9 @@ $("document").ready(() => {
 
   // Icon & nav change on scroll
 
+  const navImg = document.getElementById("nav-img");
+  const navBar = document.getElementById("nav-bar");
+
   window.onscroll = function () {
     scroll();
   };
@@ -21,28 +24,26 @@ $("document").ready(() => {
       document.body.scrollTop > 80 ||
       document.documentElement.scrollTop > 80
     ) {
-      document.getElementById("nav-img").style.filter = "invert(0)";
-      document.getElementById("nav-img").style.maxWidth = "100px";
-      document.getElementById("nav-img").src = "images/FZ6/revs.png";
-      document.getElementById("nav-bar").style.backgroundColor =
-        "rgba(0,0,0, 1)";
-      document.getElementById("nav-bar").style.padding = "10px 100px";
+      navImg.style.filter = "invert(0)";
+      navImg.style.maxWidth = "100px";
+      navImg.src = "images/FZ6/revs.png";
+      navBar.style.backgroundColor = "rgba(0,0,0, 1)";
+      navBar.style.padding = "10px 100px";
 
       if (window.innerWidth < 1280) {
-        document.getElementById("nav-bar").style.padding = "30px 70px";
+        navBar.style.padding = "30px 40px";
       } else if (window.innerWidth < 540) {
-        document.getElementById("nav-bar").style.padding = "20px 40px";
+        navBar.style.padding = "20px 40px";
       }
     } else {
-      document.getElementById("nav-img").src = "images/FZ6/ymhicon.png";
-      document.getElementById("nav-bar").style.backgroundColor =
-        "rgb(0,0,0, 0.5)";
-      document.getElementById("nav-bar").style.padding = "20px 100px";
+      navImg.src = "images/FZ6/ymhicon.png";
+      navBar.style.backgroundColor = "rgba(0,0,0, 0.5)";
+      navBar.style.padding = "20px 100px";
 
       if (window.innerWidth < 1280) {
-        document.getElementById("nav-bar").style.padding = "20px 70px";
+        navBar.style.padding = "20px 40px";
       } else if (window.innerWidth < 540) {
-        document.getElementById("nav-bar").style.padding = "10px 40px";
+        navBar.style.padding = "10px 40px";
       }
     }
   }
@@ -65,7 +66,7 @@ $("document").ready(() => {
   console.log("sliderItemWidth", sliderItemWidth);
   console.log("slidesLength", slidesLength);
 
-  //  dots
+  //  Change slide with dots
 
   [...dots].forEach((dot) =>
     dot.addEventListener("click", function (i) {
@@ -81,6 +82,8 @@ $("document").ready(() => {
       console.log("index", slideIndex);
     })
   );
+
+  // change slides with buttons
 
   next.addEventListener("click", function () {
     changeSlide(1);
@@ -121,4 +124,77 @@ $("document").ready(() => {
     [...dots].forEach((item) => item.classList.remove("active"));
     dots[index].classList.add("active");
   }
+
+  // lightbox
+
+  const images = document.querySelectorAll(".slider-main img");
+  images.forEach((item) => item.addEventListener("click", zoomImage));
+
+  function zoomImage(event) {
+    console.log(event.target);
+
+    const image = event.target.getAttribute("src");
+
+    console.log("source =", image);
+
+    const imageToZoom = `<div class="lightbox">
+      <div class="lightbox-content">
+      <ion-icon id="lightbox-prev" name="chevron-back-outline"></ion-icon>
+        <img class="lightbox-image" src="${image}" alt=""> 
+        <ion-icon id="lightbox-next" name="chevron-forward-outline"></ion-icon>
+      </div>
+    </div>`;
+
+    document.body.insertAdjacentHTML("beforeend", imageToZoom);
+  }
+
+  // close lightbox
+
+  let index2 = 0;
+  document.body.addEventListener("click", function (e) {
+    const lightImage = document.querySelector(".lightbox-image");
+    let lightImageSrc = "";
+
+    if (e.target.matches(".lightbox")) {
+      // remove lightbox
+      e.target.parentNode.removeChild(e.target);
+    } else if (e.target.matches("#lightbox-next")) {
+      // handle next button
+      lightImageSrc = lightImage.getAttribute("src");
+
+      index2 = [...images].findIndex(
+        (item) => item.getAttribute("src") === lightImageSrc
+      );
+
+      index2 = index2 + 1;
+
+      console.log("index =", index2);
+
+      if (index2 > images.length - 1) {
+        index2 = 0;
+      }
+
+      const newimageSrc = [...images][index2].getAttribute("src");
+      lightImage.setAttribute("src", newimageSrc);
+    } else if (e.target.matches("#lightbox-prev")) {
+      // handle prev button
+
+      lightImageSrc = lightImage.getAttribute("src");
+
+      index2 = [...images].findIndex(
+        (item) => item.getAttribute("src") === lightImageSrc
+      );
+
+      index2 = index2 - 1;
+
+      console.log("index =", index2);
+
+      if (index2 < 0) {
+        index2 = images.length - 1;
+      }
+
+      const newimageSrc = [...images][index2].getAttribute("src");
+      lightImage.setAttribute("src", newimageSrc);
+    }
+  });
 });
