312f436976233889fb694760397839f53d7d74ea
timfung <lonelyblue89@gmail.com>
2022-03-28 17:58:27 +0700

    add bttop btn

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 
-console.log('script!');
+console.log("script!");
 
 $(document).ready(() => {
   AOS.init({
@@ -10,33 +10,58 @@ $(document).ready(() => {
 
   // Scrolltop
 
-  $('.scroll').click(() => {
-    $('html, body').animate(
+  $(".scroll").click(() => {
+    $("html, body").animate(
       {
-        scrollTop: $('.aboutme').offset().top + 50,
+        scrollTop: $(".aboutme").offset().top + 50,
       },
-      1000,
+      1000
+    );
+  });
+
+  // back to top btn
+  window.onscroll = function () {
+    onScroll();
+  };
+
+  function onScroll() {
+    if (
+      document.body.scrollTop > 60 ||
+      document.documentElement.scrollTop > 60
+    ) {
+      $(".bttop").css("display", "block");
+    } else {
+      $(".bttop").css("display", "none");
+    }
+  }
+
+  $(".bttop").click(() => {
+    $("html, body").animate(
+      {
+        scrollTop: $("#bg-header").offset().top,
+      },
+      1000
     );
   });
 
   // scroll to work page
 
-  
-  $('#scroll-work').click(() => {
-    
-    $('html, body').animate({
-      scrollTop: $('.mywork').offset().top,
-    }, 1200)
-    
+  $("#scroll-work").click(() => {
+    $("html, body").animate(
+      {
+        scrollTop: $(".mywork").offset().top,
+      },
+      1200
+    );
   });
 
   // add responsive button
 
-  const menuToggle = document.querySelector('.toggle');
-  const bg = document.querySelector('.bg');
+  const menuToggle = document.querySelector(".toggle");
+  const bg = document.querySelector(".bg");
 
-  menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active');
-    bg.classList.toggle('active');
+  menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("active");
+    bg.classList.toggle("active");
   });
 });
